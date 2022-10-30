db7eb40f70e0a822b9be841c1be3ca570f519b76
H4de5 <48757788+H4de5-7@users.noreply.github.com>
2022-10-30 20:34:23 +0800

    Add files via upload

---
+++ Csharp/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Text;
+
+namespace TestExe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //byte[] commandEncoded = Convert.FromBase64String(args[0]);
+            //string command = Convert.FromBase64String(Encoding.UTF8.GetString(commandEncoded));
+            //string command = Encoding.UTF8.GetString(commandEncoded);
+            Runspace mySpace = RunspaceFactory.CreateRunspace();
+            mySpace.Open();
+            Pipeline myPipeLine = mySpace.CreatePipeline();
+            Console.WriteLine(args[0].Replace("+", " "));
+            myPipeLine.Commands.AddScript(args[0].Replace("+"," "));
+            Collection<PSObject> outputs = myPipeLine.Invoke();
+            mySpace.Close();
+
+            System.Text.StringBuilder sb = new StringBuilder();
+
+            foreach (PSObject pobject in outputs)
+            {
+
+                sb.AppendLine(pobject.ToString());
+
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
